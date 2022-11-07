66069cde88dd84c54bfd8ae8387ea403d11c2e69
Ivan <ivanbrazas@gmail.com>
2022-11-07 07:58:17 -0300

    finishes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import {useCallback, useEffect, useState} from "react";
 
 import api from "./api";
 
+// ## Nivel 1
+// - [X] Se repiten letras de manera muy rara en nuestra aplicación al usarla.
+
+// ## Nivel 2
+// - [X] De que manera puedo mejorar la creación del estado `words`?
+
+// ## Nivel 3
+// - [X] Hay una mala práctica al manejar el estado, cual es? Por que?
+
+// ## Extra
+// - [ ] Podemos reemplazar nuestros 4 estados con un reducer?
+
+
+
+
 function App() {
   const [answer, setAnswer] = useState<string>("");
   const [isLoading, toggleLoading] = useState<boolean>(true);
   const [turn, setTurn] = useState<number>(0);
   const [status, setStatus] = useState<"playing" | "finished">("playing");
-  const [words, setWords] = useState<string[][]>(
+  const [words, setWords] = useState<string[][]>( () => 
     Array.from({length: 6}, () => new Array(5).fill("")),
   );
 
@@ -42,9 +57,12 @@ function App() {
               firstEmptyIndex = words[turn].length;
             }
 
-            words[turn][firstEmptyIndex - 1] = "";
+            const hola = words[turn].map((item,index) => index === firstEmptyIndex - 1 ? "" : item )
 
-            setWords(words.slice());
+            const newMatrix = words.map((item, index) => index === turn ? hola: item)
+          setWords(newMatrix)
+            // words[turn][firstEmptyIndex - 1] = "";
+
 
             return;
           }
@@ -72,7 +90,9 @@ function App() {
   );
 
   useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keydown", handleKeyDown)
+
+    return () => window.removeEventListener("keydown", handleKeyDown)
   }, [handleKeyDown]);
 
   if (isLoading) return "Cargando...";
